373e986915f89883ccda7d83ed6560e918ec1f2e
dsemsem <105078892+dsemsem@users.noreply.github.com>
2022-05-09 10:16:41 -0700

    Update TTC_processing.py

--- TTC_processing.py
+++ TTC_processing.py
@@ -1,20 +1,20 @@
 # import tools
-from nltk.tokenize import word_tokenize, TreebankWordTokenizer, PunktSentenceTokenizer
+from nltk.tokenize import word_tokenize, PunktSentenceTokenizer
 from nltk.stem import WordNetLemmatizer
 from nltk import pos_tag, RegexpParser
 # regex
 import re
 
-
+# function to tokenize by word and sentence
 def word_sentence_tokenize(text):
   
-  # create a PunktSentenceTokenizer
+  # initialize PunktSentenceTokenizer
   sentence_tokenizer = PunktSentenceTokenizer(text)
   
   # sentence tokenize text
   sentence_tokenized = sentence_tokenizer.tokenize(text)
   
-  # create a list to hold word tokenized sentences
+  # creates a list to hold word tokenized sentences
   word_tokenized = list()
   
   # for-loop through each tokenized sentence in sentence_tokenized
@@ -28,7 +28,7 @@ def word_sentence_tokenize(text):
 with open(r".\tale2.txt") as ttc:
     text = ttc.read().lower()
 
-#convert line breaks to spaces using re library
+# convert line breaks to spaces using re library
 text = re.sub('\n', ' ', text)
 # print(text)
 
@@ -39,7 +39,7 @@ word_tokenized_text = word_sentence_tokenize(text)
 # empty list for part of speech tagging
 pos_tagged_text = list()
 
-# create a for loop through each word tokenized sentence here
+# for-loop through each word tokenized sentence here
 for tagged_sentence in word_tokenized_text:
   # part-of-speech tag each sentence and append to list of pos-tagged sentences here
   pos_tagged_text.append(pos_tag(tagged_sentence))
@@ -48,8 +48,10 @@ for tagged_sentence in word_tokenized_text:
 # initialization of WordNetLemmatizer
 lemmatizer = WordNetLemmatizer()
 
+# list comprehension lemmatize words in the list of POS tagged tokens.
 lemmatized = [lemmatizer.lemmatize(token, pos_tag(token)) for token in word_tokenized_text]
 print("lemmatized data:", lemmatized)
 
+# writing my progess
 #with open('notlemmatized.txt', 'w') as f:
     f.write(str(pos_tagged_text))
